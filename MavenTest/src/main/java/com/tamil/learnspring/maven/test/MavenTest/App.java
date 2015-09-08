707ab46f0279ea19523d8df9b2004968d1aa41af
Murugan_Nagarajan <Murugan_Nagarajan@CHNMCT105003L.ad.infosys.com>
2015-09-09 00:47:43 +0530

    Sample Project created to test a successfull Maven installation/build

---
+++ MavenTest/src/main/java/com/tamil/learnspring/maven/test/MavenTest/App.java
@@ -0,0 +1,13 @@
+package com.tamil.learnspring.maven.test.MavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
